496084f0114a5d5518f3bdfe11f86910887a66d1
AdiFischer <adifischer1@gmail.com>
2022-10-24 15:10:37 -0400

    adding external files

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const family = [
+    {
+        dad: true,
+        name: 'joe',
+        job:'mechanic,'
+    },
+    {
+        kid1: 'mia',
+        pet: 'fish',
+    },
+]
+console.log('hey class here is my fam', family)
+
+document.getElementById('contact').style.color = 'purple'
+
+const handleColorChange = () => {
+    console.log('changing color')
+}
+
+const handleAlert = () => {
+   alert('Alerting the class')
+}
